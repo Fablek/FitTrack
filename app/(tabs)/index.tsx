7a38f28ddffdaeb12bfd145c142330d1f934c8d4
Sebastian Pytka <pytka96@gmail.com>
2026-05-06 18:13:04 +0200

    feat: add calendar comp for index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CalendarStrip from "../../components/CalendarStrip";
 import Header from "../../components/Header";
 
 export default function HomeScreen() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const handleDateChange = (date: Date) => {
+    setCurrentDate(date);
+    // Here data from AsyncStorage for this date
+  };
+
   return (
     <View style={styles.container}>
       <Header />
+
+      <CalendarStrip onDateSelect={handleDateChange} />
+
+      <View style={styles.content}>
+        <Text style={styles.debugText}>
+          Wybrana data: {currentDate.toLocaleDateString()}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -14,5 +30,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F8F9FA",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  debugText: {
+    color: "#666",
+    fontStyle: "italic",
   },
 });
